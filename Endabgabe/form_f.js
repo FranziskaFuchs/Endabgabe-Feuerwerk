9c01d51ebfdb40fc090aa35889ea96c010623862
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2021-02-07 15:48:46 +0100

    1

--- Endabgabe/form_f.js
+++ Endabgabe/form_f.js
@@ -2,15 +2,16 @@ var Feuerwerk;
 (function (Feuerwerk) {
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
+        console.log("start_handleLoad");
         var form = document.querySelector("div#form");
         var slider = document.querySelector("input#Amount");
         form.addEventListener("change", handleChange);
         slider.addEventListener("input", displayAmount);
     }
     function handleChange(_event) {
-        //console.log("_event");
-        var rocketname = document.querySelector("input#Rocketname");
-        console.log(rocketname.value);
+        // console.log("_event");
+        var formData = new FormData(document.forms[0]);
+        console.log(formData);
     }
     function displayAmount(_event) {
         console.log("displayAmount");
---
+++ Endabgabe/form_f.ts
@@ -0,0 +1,31 @@
+namespace Feuerwerk{
+    window.addEventListener("load", handleLoad);
+  
+  function handleLoad(_event: Event):void {
+   console.log("start_handleLoad");
+  
+    let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+    let slider: HTMLInputElement = <HTMLInputElement>document.querySelector("input#Amount");
+  
+    form.addEventListener("change",handleChange);
+    slider.addEventListener("input", displayAmount);
+  
+  }
+  
+  function handleChange(_event:Event): void {
+  // console.log("_event");
+  
+  let formData: FormData = new FormData(document.forms[0]);
+  console.log(formData);
+    }
+  
+  function displayAmount(_event:Event): void{
+    console.log("displayAmount");
+  
+   let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
+   let Amount: string = (<HTMLInputElement>_event.target).value;
+   progress.value = parseFloat(Amount);
+  }
+   
+   
+   }
