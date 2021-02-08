5c17610e795128f5488f7148cc77492fc50898d8
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2021-02-08 16:16:32 +0100

    2

--- Endabgabe/form_f.js
+++ Endabgabe/form_f.js
@@ -3,15 +3,20 @@ var Feuerwerk;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("start_handleLoad");
-        var form = document.querySelector("div#form");
+        var form = document.querySelector("form");
         var slider = document.querySelector("input#Amount");
         form.addEventListener("change", handleChange);
         slider.addEventListener("input", displayAmount);
     }
     function handleChange(_event) {
-        // console.log("_event");
+        var RocketList = document.querySelector("div#RocketList");
+        RocketList.innerHTML = "";
         var formData = new FormData(document.forms[0]);
-        console.log(formData);
+        for (var _i = 0, formData_1 = formData; _i < formData_1.length; _i++) {
+            var entry = formData_1[_i];
+            var item = document.querySelector("[value='" + entry[1] + " ']");
+            RocketList.innerHTML += item.name;
+        }
     }
     function displayAmount(_event) {
         console.log("displayAmount");
--- Endabgabe/form_f.ts
+++ Endabgabe/form_f.ts
@@ -1,31 +1,53 @@
-namespace Feuerwerk{
-    window.addEventListener("load", handleLoad);
-  
-  function handleLoad(_event: Event):void {
-   console.log("start_handleLoad");
-  
-    let form: HTMLDivElement = <HTMLDivElement>document.querySelector("div#form");
+namespace Feuerwerk {
+  window.addEventListener("load", handleLoad);
+  let form: HTMLFormElement;
+
+  function handleLoad(_event: Event): void {
+    console.log("start_handleLoad");
+
+    form = <HTMLFormElement>document.querySelector("form");
     let slider: HTMLInputElement = <HTMLInputElement>document.querySelector("input#Amount");
-  
-    form.addEventListener("change",handleChange);
+
+    form.addEventListener("change", handleChange);
     slider.addEventListener("input", displayAmount);
-  
+
+    displayRocketList();
   }
-  
-  function handleChange(_event:Event): void {
-  // console.log("_event");
-  
-  let formData: FormData = new FormData(document.forms[0]);
-  console.log(formData);
+
+ function sendRocketList(_event: Event): void {
+    console.log("send RocketList");
+
+    let formData: FormData = new FormData(form);
+
+  }
+
+
+  function handleChange(_event: Event): void {
+
+    displayRocketList();
+  }
+
+  function displayRocketList(): void {
+    let RocketList: HTMLDivElement = <HTMLDivElement>document.querySelector("div#RocketList");
+    RocketList.innerHTML = "";
+
+    let formData: FormData = new FormData(form);
+
+    for (let entry of formData) {
+      let selector: string = "[vale= '" + entry[1] + "']";
+      let item: HTMLInputElement = <HTMLInputElement>document.querySelector(selector);
+
+      RocketList.innerHTML += item.value;
     }
-  
-  function displayAmount(_event:Event): void{
-    console.log("displayAmount");
-  
-   let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
-   let Amount: string = (<HTMLInputElement>_event.target).value;
-   progress.value = parseFloat(Amount);
   }
-   
-   
-   }
+
+}
+
+function displayAmount(_event: Event): void {
+  console.log("displayAmount");
+
+  let progress: HTMLProgressElement = <HTMLProgressElement>document.querySelector("progress");
+  let Amount: string = (<HTMLInputElement>_event.target).value;
+  progress.value = parseFloat(Amount);
+}
+
