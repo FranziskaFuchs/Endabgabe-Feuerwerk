25bf84a61c3a2c8bb95d5e22614ae523dcc34584
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2021-02-09 23:41:26 +0100

    1

--- Endabgabe/form_f.js
+++ Endabgabe/form_f.js
@@ -4,26 +4,31 @@ var Feuerwerk;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         console.log("start_handleLoad");
+        Feuerwerk.generateContent(data);
         let form = document.querySelector("form#formular");
         let slider = document.querySelector("input#Amount");
         form.addEventListener("change", handleChange);
         slider.addEventListener("input", displayAmount);
+        displayRocketList();
     }
     function handleChange(_event) {
+        displayRocketList();
+    }
+    function displayRocketList() {
         let RocketList = document.querySelector("div#RocketList");
         RocketList.innerHTML = "";
         let formData = new FormData(document.forms[0]);
         for (let entry of formData.entries()) {
-            console.log(entry);
-            //   let item: HTMLInputElement = <HTMLInputElement>document.querySelector("[value='" + entry[1] + "']")
-            //   RocketList.innerHTML += item.name;
+            //console.log(entry);
+            let selector = "[value='" + entry[1] + "']";
+            let item = document.querySelector(selector);
+            RocketList.innerHTML += item.name;
         }
-    }
-    function displayAmount(_event) {
-        console.log("displayAmount");
-        let progress = document.querySelector("progress");
-        let Amount = _event.target.value;
-        progress.value = parseFloat(Amount);
+        function displayAmount(_event) {
+            let progress = document.querySelector("progress");
+            let Amount = _event.target.value;
+            progress.value = parseFloat(Amount);
+        }
     }
 })(Feuerwerk || (Feuerwerk = {}));
 //# sourceMappingURL=form_f.js.map
