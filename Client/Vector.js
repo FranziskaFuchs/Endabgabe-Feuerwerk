5112863188fe7ae52cfcd2a63f7dd884f799ea97
FranziskaFuchs <48830900+FranziskaFuchs@users.noreply.github.com>
2021-02-15 02:12:19 +0100

    1

--- Client/Vector.js
+++ Client/Vector.js
@@ -17,11 +17,11 @@ var Feuerwerk;
         //     tempVector.set(_v0.x - _v1.x, _v0.y - _v1.y);
         //      return tempVector;
         //   }
-        //  public static getSum(_v0: Vector, _v1: Vector): Vector {
-        //      let tempVector: Vector = new Vector(0, 0);
-        //     tempVector.set(_v0.x + _v1.x, _v0.y + _v1.y);
-        //      return tempVector;
-        //  }
+        static getSum(_v0, _v1) {
+            let tempVector = new Vector(0, 0);
+            tempVector.set(_v0.x + _v1.x, _v0.y + _v1.y);
+            return tempVector;
+        }
         static getScaled(_v, _scale) {
             let tempVector = new Vector(0, 0);
             tempVector.set(_v.x * _scale, _v.y * _scale);
--- Client/Vector.ts
+++ Client/Vector.ts
@@ -23,11 +23,11 @@ namespace Feuerwerk {
         //      return tempVector;
         //   }
 
-        //  public static getSum(_v0: Vector, _v1: Vector): Vector {
-        //      let tempVector: Vector = new Vector(0, 0);
-        //     tempVector.set(_v0.x + _v1.x, _v0.y + _v1.y);
-        //      return tempVector;
-        //  }
+          public static getSum(_v0: Vector, _v1: Vector): Vector {
+              let tempVector: Vector = new Vector(0, 0);
+             tempVector.set(_v0.x + _v1.x, _v0.y + _v1.y);
+              return tempVector;
+         }
         public static getScaled(_v: Vector, _scale: number): Vector {
             let tempVector: Vector = new Vector(0, 0);
             tempVector.set(_v.x * _scale, _v.y * _scale);
